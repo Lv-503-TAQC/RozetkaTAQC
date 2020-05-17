42fa6be767c1414efc86a0cfc25aef759ac98d2a
Taras Malynovskyi <tarasmslynovskyy@gmail.com>
2020-05-17 22:35:31 +0300

    Added checker for popup banner
    Refactor locator for adding to cart

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 
 import static locators.AllAboutGoodsLocators.ADD_TO_CART_BUTTON;
-import static locators.ProductReviewsLocators.PRODUCT_REVIEW_DROPDOWN_SORT;
-import static locators.ProductReviewsLocators.PRODUCT_REVIEW_TAB;
+import static locators.ProductReviewsLocators.*;
 import static utils.Constants.*;
 
 public class Main {
+    public static final String POPUP_BANNER_CLOSE_BUTTON = "//*[@class = 'exponea-close-cross']";
+
     public static void main(String[] args) throws InterruptedException {
         System.setProperty(KEY_TO_WINDOWS_CHROME_DRIVER, PATH_TO_WINDOWS_CHROME_DRIVER);
 
@@ -24,6 +25,11 @@ public class Main {
 
             driver.findElement(By.xpath(PRODUCT_REVIEW_TAB)).click();
             Thread.sleep(2000);
+
+            boolean displayed = driver.findElement(By.xpath(POPUP_BANNER_CLOSE_BUTTON)).isDisplayed();
+            if (displayed) {
+                driver.findElement(By.xpath(POPUP_BANNER_CLOSE_BUTTON)).click();
+            }
 
             driver.findElement(By.xpath(ADD_TO_CART_BUTTON)).click();
             Thread.sleep(2000);
