849d0ae10da332b7aa0920644e7c513551b367ef
ilchenkoliuba <ilchenkoliuba@ukr.net>
2020-05-17 16:14:47 +0300

    New locators added.

---
+++ src/main/java/locators/OnCreditModalLocators.java
@@ -0,0 +1,17 @@
+package locators;
+
+public class OnCreditModalLocators {
+    public static final String CLOSE_THE_MODAL = ".modal__close";
+    public static final String STANDARD_CREDIT_SELECT = "//*[contains (text(), 'Стандарт')]//following::div[1]//select";
+    public static final String STANDARD_CREDIT_BUTTON = "//*[contains (text(), 'Стандарт')]//following::div[2]//button";
+    public static final String ALPHA_CREDIT_SELECT = "//*[contains (text(), 'Альфа')]//following::div[1]//select";
+    public static final String ALPHA_CREDIT_BUTTON = "//*[contains (text(), 'Альфа')]//following::div[2]//button";
+    public static final String MONO_CREDIT_SELECT = "//*[contains (text(), 'monobank')]//following::div[1]//select";
+    public static final String MONO_CREDIT_BUTTON = "//*[contains (text(), 'monobank')]//following::div[2]//button";
+    public static final String THREE_MONTH_CONCES_SELECT = "//*[contains (text(), '0,01')][contains (text(), '3')]//following::div[1]//select";
+    public static final String THREE_MONTH_CONCES_BUTTON = "//*[contains (text(), '0,01')][contains (text(), '3')]//following::div[2]//button";
+    public static final String TEN_MONTH_CONCES_SELECT = "//*[contains (text(), '0,01')][contains (text(), '10')]//following::div[1]//select";
+    public static final String TEN_MONTH_CONCES_BUTTON = "//*[contains (text(), '0,01')][contains (text(), '10')]//following::div[2]//button";
+    public static final String PAY_IN_PARTS_SELECT = "//*[contains (text(), 'Оплата')]//following::div[1]//select";
+    public static final String PAY_IN_PARTS_BUTTON = "//*[contains (text(), 'Оплата')]//following::div[2]//button";
+}
