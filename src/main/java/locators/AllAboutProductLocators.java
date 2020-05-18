2af6b7492b1f7f5aa685d72cded5170612a0333e
ilchenkoliuba <ilchenkoliuba@ukr.net>
2020-05-18 12:37:11 +0300

    TextField pageelement added; pageobject package created and pageobgects added: Basepageobject, ActionsWithProductsBlock, AllAboutItemTab.

--- src/main/java/locators/AllAboutProductLocators.java
+++ src/main/java/locators/AllAboutProductLocators.java
@@ -3,9 +3,9 @@ package locators;
 public class AllAboutProductLocators {
 
     public static final String PRODUCT_OPTIONS = "//*[@class = 'product-main__parameters']//li";
-    public static final String PRODUCT_AVAILABILITY = ".product__status";
+    //public static final String PRODUCT_AVAILABILITY = ".product__status";
     //Locator to find the list of all the additional cervices
-    public static final String ADDITIONAL_CERVICES_LIST = "//*[@class = 'additional-services']//li";
+    public static final String ADDITIONAL_SERVICES_LIST = "//*[@class = 'additional-services']//li";
     //Locators to find the desirable element of the desirable additional service
     public static final String ADDITIONAL_CERVICE_CHECKBOX = "//*[@class = 'additional-service__checkbox']";
     public static final String ADDITIONAL_CERVICE_INFO = "//*[@class = 'additional-service__checkbox']//button";
@@ -17,6 +17,8 @@ public class AllAboutProductLocators {
     //Locator to find the list of all the pictograms
     public static final String BONUS_PICTOGRAMS_LIST = "//*[@class = 'product-about__right']//li//img";
     public static final String DELIVERY_CITY_MODAL = "//*[@class = 'product-about__block-heading']//button";
+    //Locators for delivery city modal window should be added here
     public static final String PREMIUM_DELIVERY_LINK = "//*[contains (@class, 'product-premium__button')]";
     public static final String PRODUCT_PICTOGRAMS_LIST = "//*[@class = 'product-about__sticky']//app-product-pictogram-list//li//img";
+    public static final String PRODUCT_INFO_BRIEF = ".product-about__brief";
 }
---
+++ src/main/java/pageobjects/ActionsWithProductsBlock.java
@@ -0,0 +1,50 @@
+package pageobjects;
+
+import locators.ActionsWithProductsLocators;
+import locators.AllAboutProductLocators;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import pageelements.Button;
+import pageelements.TextField;
+
+import java.util.List;
+
+public class ActionsWithProductsBlock extends BasePageObject{
+
+    private Button addToCart;
+    private Button addToCompareList;
+    private Button addToWishList;
+    private String creditInfo;
+    private Button buyOnCredit;
+
+    public ActionsWithProductsBlock(WebDriver driver) {
+        super(driver);
+    }
+
+    public ActionsWithProductsBlock addToCart() {
+        addToCart = new Button(driver, ActionsWithProductsLocators.ADD_TO_CART_BUTTON).click();
+        return this;
+    }
+
+    public ActionsWithProductsBlock addToCompareList() {
+        addToCompareList = new Button(driver, ActionsWithProductsLocators.ADD_TO_COMPARE_LIST_BUTTON).click();
+        return this;
+    }
+
+    public ActionsWithProductsBlock addToWishList() {
+        addToWishList = new Button(driver, ActionsWithProductsLocators.ADD_TO_WISH_LIST).click();
+        return this;
+
+    }
+
+    public String getCreditInfo(){
+        creditInfo = new TextField(driver,ActionsWithProductsLocators.CREDIT_INFO).getText();
+        return creditInfo;
+
+    }
+
+    public ActionsWithProductsBlock setDeliveryCity() {
+        buyOnCredit = new Button(driver, ActionsWithProductsLocators.BUY_ON_CREDIT).click();
+        return this;
+    }
+}
