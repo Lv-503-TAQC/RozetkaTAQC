7496fc185acd7854ca29700fc60b5501b3bd0a9b
TarasMalynovskyy <tarasmalynovskyy@gmail.com>
2020-05-24 12:20:09 +0300

    Add SingleCommentPageObject,
    refactor locators.

---
+++ src/main/java/pageobjects/NewCommentPopupDialogPageObject.java
@@ -0,0 +1,9 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class NewCommentPopupDialogPageObject extends BasePageObject {
+    public NewCommentPopupDialogPageObject(WebDriver driver) {
+        super(driver);
+    }
+}
